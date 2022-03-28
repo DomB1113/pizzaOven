c89b64c15f819e47bc539d8a68d887e407e89e5d
dom b <bellybuchelli1234@gmail.com>
2022-03-28 09:39:39 -0400

    commit

---
+++ script.js
@@ -0,0 +1,84 @@
+function pizzaoven(crustType, sauceType, cheeses, toppings){
+    var pizza = {};
+    pizza.crustType = crustType
+    pizza.sauceType = sauceType
+    pizza.cheeses = cheeses
+    pizza.toppings = toppings
+    return pizza;
+}
+var domsPizza = pizzaoven("cheese filled", "buffalo hot", "extra cheese", "meat lovers")
+console.log(domsPizza)
+var herPizza = pizzaoven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"])
+console.log(herPizza)
+var hisPizza = pizzaoven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"])
+console.log(hisPizza)
+
+// for random pizza 
+
+var crustTypes = [
+    "deep dish",
+    "hand tossed",
+    "thin and crispy",
+    "cauliflower",
+    "gluten free",
+    "hawaiian bread"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce",
+    "nacho cheese",
+    "tikka masala"
+];
+
+var cheeses = [
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "parmesan",
+    "vegan cheese"
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "corn",
+    "olives",
+    "bell peppers",
+    "onions",
+    "mushrooms",
+    "anchovies"
+];
+
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustTypes);
+    pizza.sauceType = randomPick(sauceTypes);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log(randomPizza());
